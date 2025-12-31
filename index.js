21b31bc1bfd15112321988d8636135a480b6f79b
mohammed.mahmoud <152339587+nour-el-dien@users.noreply.github.com>
2025-12-31 14:40:07 +0200

    Refactor updateTime to use setTimeout
    Replaced setInterval with setTimeout for updateTime function.

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 let myele = document.querySelector("h1");
 
 function updateTime(){
-
-
     let data = new Date()
-
     let hours = data.getHours();
     let minutes = data.getMinutes();
     let seconds = data.getSeconds();
@@ -13,13 +10,12 @@ function updateTime(){
     let year = data.getUTCFullYear();
     let day = data.getUTCDate();
 
-    console.log(data);
     myele.innerHTML = `${year}-${month}-${day} <br>
      ${hours}:${minutes}:${seconds}`;
+    setTimeout(updateTime, 1000);
+    
+
 }
 
 
-setInterval (function(){
 updateTime()
- },1000)
- updateTime()
